063e18664981074c70d26d0569817db434abcf5b
rasgrin <stefanrogic@protonmail.com>
2023-07-20 18:19:03 +0200

    fixed banner and product page margin

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,6 +4,8 @@ import { useRef } from "react";
 import { motion, useInView } from "framer-motion";
 import { Link } from "react-router-dom";
 
+import bannerImg from "../../assets/banner.jpg";
+
 const variants = {
   invisible: { opacity: 0, x: -100 },
   visible: { opacity: 1, x: 0 },
@@ -16,7 +18,7 @@ const Banner = () => {
   return (
     <Link to="/products/categories/vitamins">
       <motion.div className="default-margin" ref={ref} animate={isInView ? "visible" : "invisible"} variants={variants} transition={{ duration: 1 }}>
-        <div className="home-banner">
+        <div className="home-banner" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('${bannerImg}')` }}>
           <h1>UNLEASH YOUR INNER VITALITY WITH OUR PREMIUM VITAMINS AND MINERALS</h1>
         </div>
       </motion.div>
