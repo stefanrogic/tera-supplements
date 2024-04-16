31b682d5a84e6e9abff40000fb272b2cb2ab4139
stefanrogc <stefanrogic@protonmail.com>
2024-04-16 17:47:12 +0200

    new components for home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,16 +13,78 @@ const Home = () => {
           <div className="brand"></div>
         </section>
 
-        <section className="product-cta">
+        <section className="product-cta-section">
           <div className="category-card">
             <div className="img-container"></div>
-            <h1>CATEGORY</h1>
+
+            <div>
+              <h2>CATEGORY</h2>
+              <span>About this category</span>
+            </div>
           </div>
           <div className="category-card">
             <div className="img-container"></div>
-            <h1>CATEGORY</h1>
+
+            <div>
+              <h2>CATEGORY</h2>
+              <span>About this category</span>
+            </div>
           </div>
         </section>
+
+        <section className="featured-section">
+          <div className="featured-section-grid">
+            <div className="featured-product-card">
+              <div className="img-container"></div>
+              <div>
+                <h2>CATEGORY</h2>
+                <span>About this category</span>
+              </div>
+            </div>
+            <div className="featured-product-card">
+              <div className="img-container"></div>
+              <div>
+                <h2>CATEGORY</h2>
+                <span>About this category</span>
+              </div>
+            </div>
+            <div className="featured-product-card">
+              <div className="img-container"></div>
+              <div>
+                <h2>CATEGORY</h2>
+                <span>About this category</span>
+              </div>
+            </div>
+            <div className="featured-product-card">
+              <div className="img-container"></div>
+              <div>
+                <h2>CATEGORY</h2>
+                <span>About this category</span>
+              </div>
+            </div>
+          </div>
+
+          <button>SHOP MORE</button>
+        </section>
+
+        <section className="home-banner-cta"></section>
+
+        <section className="featured-section">
+          <div className="featured-section-grid  alt">
+            <div className="featured-category-card">
+              <div className="img-container"></div>
+            </div>
+            <div className="featured-category-card">
+              <div className="img-container"></div>
+            </div>
+            <div className="featured-category-card">
+              <div className="img-container"></div>
+            </div>
+          </div>
+        </section>
+
+        {/* TEMP */}
+        <br />
       </div>
     </div>
   );
